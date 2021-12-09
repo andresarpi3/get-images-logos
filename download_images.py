cdb6258d45bc9a4947bdf89b59284554a962d5bd
Andres Arpi <andresarpi3@gmail.com>
2021-12-09 12:32:34 -0300

    M

--- download_images.py
+++ download_images.py
@@ -1,3 +1,4 @@
+import tqdm
 import re
 import os
 import urllib.request
@@ -17,13 +18,33 @@ params = {
 }
 
 limit_searches = None
+outpath = None
 
 def convert_to_brand_path(brand):
     return re.sub(r"[^a-zA-z]", "-", brand)
 
+def download_image(in_):
+    global outpath
+    image_link, brand = in_
+    brand_path = os.path.join(outpath, convert_to_brand_path(brand))
+
+    try:
+        raw_img = urllib.request.urlopen(image_link).read()
+
+        image_name = urllib.parse.urlsplit(image_link).path.split("/")[-1].split(".")[0]
+        image_name = image_name[:min(12, len(image_name))] + ".jpg"
+
+        f = open(os.path.join(brand_path, image_name), 'wb')
+        f.write(raw_img)
+        f.close()
+    except Exception as e:
+        pass#print(f"Could not download {image_link}: {str(e)}.")
+
+
 
 def download_brand(brand):
     global limit_searches
+    tqdm.tqdm.write(f"Starting {brand}")
     queries = [brand, f"{brand} logo", f"{brand} ad"]
 
     images_links = set()
@@ -42,31 +63,17 @@ def download_brand(brand):
 
 
     images_links = sorted(list(images_links))[:limit_searches]
-    downloaded_images = 0
-    for image_link in images_links:
-        brand_path = os.path.join(outpath, convert_to_brand_path(brand))
 
-        try:
-            raw_img = urllib.request.urlopen(image_link).read()
+    with concurrent.futures.ThreadPoolExecutor(max_workers = 8) as executor:
+        executor.map(download_image, zip(images_links, [brand for x in range(len(images_links))]))
 
-            image_name = urllib.parse.urlsplit(image_link).path.split("/")[-1].split(".")[0]
-            image_name = image_name[:min(12, len(image_name))] + ".jpg"
-
-            f = open(os.path.join(brand_path, image_name), 'wb')
-            f.write(raw_img)
-            f.close()
-            downloaded_images += 1
-        except Exception as e:
-            pass#print(f"Could not download {image_link}: {str(e)}.")
-
-    print(f"{datetime.datetime.now()} - Brand {brand} ended downloading {downloaded_images} out of {len(images_links)}")
-
-
+    downloaded_images = len(os.listdir(os.path.join(outpath, convert_to_brand_path(brand))))
+    tqdm.tqdm.write(f"{datetime.datetime.now()} - Brand {brand} ended downloading {downloaded_images} out of {len(images_links)}")
 
 
 def download_all_brands(brands):
-    with concurrent.futures.ThreadPoolExecutor(max_workers = 1) as executor:
-        executor.map(download_brand, brands)
+    for brand in tqdm.tqdm(brands):
+        download_brand(brand)
 
 try:
     number_of_brands = int(sys.argv[1])
@@ -80,7 +87,7 @@ print(f"Will download at most {limit_searches} for {number_of_brands} brands")
 outpath = "output"
 brands_df = pd.read_csv('1000-brands.csv')
 brands = brands_df["name"] #.str.replace(r'[^a-zA-Z]', '_', regex=True)
-brands = brands[:number_of_brands]
+brands = brands[:]
 
 os.makedirs(outpath, exist_ok=True)
 for brand in brands:
